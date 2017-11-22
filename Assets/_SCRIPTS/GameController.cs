b41fbdc727fd9c3a017e2c3b2c0f31a77981e1a0
Ross Ylitalo <ross.ylitalo@gmail.com>
2017-11-22 09:03:55 -0500

    Prompts are good, all is good.

--- Assets/_SCRIPTS/GameController.cs
+++ Assets/_SCRIPTS/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -24,23 +25,28 @@ public class GameController : MonoBehaviour
         
     }
 
-    private int newNumber()
+    private int newNumber(bool appendPrompt = false)
     {
-        _prompt.text = string.Format("Enter Value Between {0} - {1}.", MIN, MAX);
-        return Random.Range(MIN, MAX) + 1;
+        if (appendPrompt)
+            _prompt.text += Environment.NewLine + string.Format("Enter Value Between {0} - {1}.", MIN, MAX);
+        else
+            _prompt.text = string.Format("Enter Value Between {0} - {1}.", MIN, MAX);
+        _input.Select();
+        return UnityEngine.Random.Range(MIN, MAX) + 1;
     }
 
     public void GetInput(string strGuess)
     {
         int guess;
 
+        Debug.Log("You Entered: " + strGuess);
         if (int.TryParse(strGuess, out guess))
         {
             ++_totalGuesses;
             if (guess == _numberToGuess)
             {
                 _prompt.text = string.Format("Congratulation! You found it in only {0} guesses!", _totalGuesses);
-                _numberToGuess = newNumber();
+                _numberToGuess = newNumber(true);
             }
             else
             {
