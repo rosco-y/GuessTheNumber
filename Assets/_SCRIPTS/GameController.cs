b2bf5678dcf7630ae980c55d61c4cc5966726aea
Ross Ylitalo <ross.ylitalo@gmail.com>
2017-11-22 08:51:41 -0500

    Modified GameController to the point where I think it should be a functional game now.

--- Assets/_SCRIPTS/GameController.cs
+++ Assets/_SCRIPTS/GameController.cs
@@ -1,16 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private const int MIN = 0;
+    private const int MAX = 100;
+
+    [SerializeField]
+    InputField _input;
+
+    [SerializeField]
+    Text _prompt;
+
+    int _numberToGuess;
+    int _totalGuesses;
+    public void Awake()
+    {
+        _totalGuesses = 0;
+        _numberToGuess = newNumber();
+        
+    }
+
+    private int newNumber()
+    {
+        _prompt.text = string.Format("Enter Value Between {0} - {1}.", MIN, MAX);
+        return Random.Range(MIN, MAX) + 1;
+    }
+
+    public void GetInput(string strGuess)
+    {
+        int guess;
+
+        if (int.TryParse(strGuess, out guess))
+        {
+            ++_totalGuesses;
+            if (guess == _numberToGuess)
+            {
+                _prompt.text = string.Format("Congratulation! You found it in only {0} guesses!", _totalGuesses);
+                _numberToGuess = newNumber();
+            }
+            else
+            {
+                if (guess < _numberToGuess)
+                {
+                    _prompt.text = "Guess too Small.";
+                }
+                else
+                    _prompt.text = "Guess too Large.";
+            }
+        }
+        else
+            _prompt.text = "Please Use Numbers.";
+
+        _input.text = "";
+        _input.Select();
+    }
+
+
 }
